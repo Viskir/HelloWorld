24fe2a38b521ad7b33dd96dcc317e25e0b50913e
Travis Kirsten <traviskay@abc.com>
2018-12-17 21:42:15 -0600

    First commit made in eclipse.

--- main.java
+++ main.java
@@ -1,6 +1,6 @@
 public class main{
   public static void main(String[] args){
     System.out.println("Hello world!");
-    System.out.println("version 1");
+    System.out.println("version 2.  Changed in eclipse");
   }
 }
